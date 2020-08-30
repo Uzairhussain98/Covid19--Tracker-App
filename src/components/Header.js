cf05e6c4507332ee7f9784b572a2b9b540e4cf97
Uzair Hussain <uzairhussain98@hotmail.com>
2020-08-30 16:25:48 +0500

    added web-animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import image from './virus.png';
+import useWebAnimations from "@wellyshen/use-web-animations";
+
 
 import './link.css'
 import {
@@ -32,6 +35,30 @@ const useStyles = makeStyles((theme) => ({
 export default function Header() {
   const classes = useStyles();
 
+
+  const { ref } = useWebAnimations({
+    keyframes: [
+      { transform: 'rotate(0)' } ,
+      { transform: 'rotate(360deg)' }
+    ],
+    timing: {
+      duration: 2000, // Run for 1000ms
+      iterations: Infinity, // Repeat once
+       direction:"alternate",// Run the animation forwards and then backwards
+      easing: "ease-in-out", // Use a fancy timing function
+  }}
+    );
+
+
+
+
+
+
+
+
+
+
+
   return (
     
 
@@ -41,6 +68,7 @@ export default function Header() {
       <AppBar position="static" color="primary">
 
         <Toolbar>
+          <img ref={ref} style={{height:"70px" , width:"70px" , marginRight:'10px' , float:"left"}} src={image} alt='virus' />
           <Typography variant="h5" className={classes.title}>
             Covid-19 Tracker
           </Typography>
